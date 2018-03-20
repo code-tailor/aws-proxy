dddbaec7d22d7f4e0721b8e25bc65fb6be95587a
Vishal Balar <vishal.balar@dreamworldsolutions.com>
2018-03-20 16:36:09 +0530

    initial commit

---
+++ src/main/java/com/dw/aws/proxy/AwsProxyApplication.java
@@ -0,0 +1,12 @@
+package com.dw.aws.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProxyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AwsProxyApplication.class, args);
+  }
+}
